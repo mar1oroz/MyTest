3fa032585c3ee468f04c9f12d5a648ebf21265f0
MarianRozumnyi <rozumnyi.marian@gmail.com>
2022-08-17 10:08:19 +0300

    First

--- Arreys4.1/Program.cs
+++ Arreys4.1/Program.cs
@@ -4,58 +4,52 @@ namespace Arreys4._1
 {
     class Program
     {
-        //Создайте метод, который принимает два аргумента, первый аргумент - типа int[] array, второй аргумент - типа int value.
-        //В теле метода реализуйте возможность добавления второго аргумента метода в массив по индексу – 0,
-        //при этом длина нового массива, должна увеличиться на 1 элемент,
-        //а элементы получаемого массива в качестве первого аргумента должны скопироваться в новый массив начиная с индекса - 1.
+
+        //Создать метод, который будет выполнять увеличение длины массива переданного в качестве аргумента, на 1 элемент.
+		//Элементы массива, должны сохранить свое значение и порядок индексов.
 
 
-        static int[] ChangeArrey(int[]arrey,int value)
+        
+        public static int Increase(int[] arrey)
         {
-            int[] Arrey3 = new int[arrey.Length + 1];
+            
+            int[] Arrey1 = new int[arrey.Length + 1];
 
-            for (int i = 0; i < arrey.Length+1; i++)
-            {
-                if (i<=0)
-                {
-                    Arrey3[i] = value;
-                }
-                if (i > 0 && i < Arrey3.Length)
-                {
-                    Arrey3[i] = arrey[i - 1];
-                }
-            }
-            return Arrey3;
-        }
-
-        static void Calculation(int[] arrey)
-        {
             for (int i = 0; i < arrey.Length; i++)
             {
-                Console.Write(arrey[i] + " ");
+                if (i <= arrey.Length + 1)
+                {
+                    Arrey1[i] = arrey[i];
+                }
+                else
+                {
+                    Arrey1[i] = 0;
+                }
             }
+            return = Arrey1;
+
+
+        }
+
+        static void Main()
+        {
+            
+            int[] array = new int[3];
+
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+            Console.WriteLine(array);
+
+            
+            int[] result = Increase(array);
+
             Console.WriteLine();
-        }
 
+            Console.ReadKey();
 
-        static void Main(string[] args)
-        {
-            int[] arrey = new int[4];
-            for (int i = 0; i < arrey.Length; i++)
-            {
-                arrey[i] = i;
-            }
-            Console.Write("Кількість елементів до зміни: ");
-            Calculation(arrey);
-
-            int value = 10;
-            int[] Arrey2 = ChangeArrey(arrey, value);
-
-            Console.Write("Кількість елементів з додаванням аргумента: ");
-
-            Calculation(Arrey2);
-
-            Console.ReadLine();
 
         }
     }
